1a02396d01fa292766a5377432b519fb5b4d8f34
Burhan-00 <burhanjaved777@gmail.com>
2026-05-14 18:52:29 +0500

    Some Changes to improve code

--- PIECE.h
+++ PIECE.h
@@ -10,47 +10,48 @@ class Board;
 class PIECE
 {
 protected:
-	COLOR color;
-	char symbol;
+	COLOR pieceColor;
+	char pieceSymbol;
 public:
-	PIECE(char s, COLOR c);
+	PIECE(char pieceChar, COLOR pieceShade);
 	char get_Symbol();
 	COLOR get_color();
-	virtual bool isValidMove(int sr, int sc, int dr, int dc, Board* b) = 0;
+	virtual bool isValidMove(int startRow, int startCol, int destRow, int destCol, Board* chessBoard) = 0;
 	virtual ~PIECE() = 0;
 };
 class PAWN :public PIECE
 {
 public:
-	PAWN(char s, COLOR c);
-	bool isValidMove(int sr, int sc, int dr, int dc, Board* b);
+	PAWN(char pawnChar, COLOR pawnColor);
+	bool isValidMove(int startRow, int startCol, int destRow, int destCol, Board* chessBoard);
 };
 class ROOK :public PIECE
 {
 public:
-	ROOK(char s, COLOR c);
-	bool isValidMove(int sr, int sc, int dr, int dc, Board* b);
+	ROOK(char rookChar, COLOR rookColor);
+	bool isValidMove(int startRow, int startCol, int destRow, int destCol, Board* chessBoard);
 };
 class KNIGHT :public PIECE
 {
 public:
-	KNIGHT(char s, COLOR c);
-	bool isValidMove(int sr, int sc, int dr, int dc, Board* b);
+	KNIGHT(char knightChar, COLOR knightColor);
+	bool isValidMove(int startRow, int startCol, int destRow, int destCol, Board* chessBoard);
 };
 class BISHOP :public PIECE
 {
 public:
-	BISHOP(char s, COLOR c);
-	bool isValidMove(int sr, int sc, int dr, int dc, Board* b);
+	BISHOP(char bishopChar, COLOR bishopColor);
+	bool isValidMove(int startRow, int startCol, int destRow, int destCol, Board* chessBoard);
 };
 class QUEEN :public PIECE
 {
 public:
-	QUEEN(char s, COLOR c);
-	bool isValidMove(int sr, int sc, int dr, int dc, Board* b);
+	QUEEN(char queenChar, COLOR queenColor);
+	bool isValidMove(int startRow, int startCol, int destRow, int destCol, Board* chessBoard);
 };
 class KING :public PIECE
 {
 public:
-	KING(char s, COLOR c);
-	bool isValidMove(int sr, int sc, int dr, int dc, Board* b);
+	KING(char kingChar, COLOR kingColor);
+	bool isValidMove(int startRow, int startCol, int destRow, int destCol, Board* chessBoard);
+};
