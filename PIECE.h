6f538d6976de3508512a44fab1f183c2ad6bc67c
Burhan-00 <burhanjaved777@gmail.com>
2026-05-14 15:15:43 +0500

    modified pieces.h
    added more functions in it.

--- PIECE.h
+++ PIECE.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #pragma once
 #include<iostream> 
 using namespace std;
@@ -55,62 +54,3 @@ class KING :public PIECE
 public:
 	KING(char s, COLOR c);
 	bool isValidMove(int sr, int sc, int dr, int dc, Board* b);
-=======
-#pragma once
-#include<iostream> 
-using namespace std;
-
-enum COLOR
-{
-	BLACK, WHITE
-};
-class Board;
-class PIECE
-{
-protected:
-	COLOR color;
-	char symbol;
-public:
-	PIECE(char s, COLOR c);
-	char get_Symbol();
-	COLOR get_color();
-	virtual bool isValidMove(int sr, int sc, int dr, int dc, Board* b) = 0;
-	virtual ~PIECE() = 0;
-};
-class PAWN :public PIECE
-{
-public:
-	PAWN(char s, COLOR c);
-	bool isValidMove(int sr, int sc, int dr, int dc, Board* b);
-};
-class ROOK :public PIECE
-{
-public:
-	ROOK(char s, COLOR c);
-	bool isValidMove(int sr, int sc, int dr, int dc, Board* b);
-};
-class KNIGHT :public PIECE
-{
-public:
-	KNIGHT(char s, COLOR c);
-	bool isValidMove(int sr, int sc, int dr, int dc, Board* b);
-};
-class BISHOP :public PIECE
-{
-public:
-	BISHOP(char s, COLOR c);
-	bool isValidMove(int sr, int sc, int dr, int dc, Board* b);
-};
-class QUEEN :public PIECE
-{
-public:
-	QUEEN(char s, COLOR c);
-	bool isValidMove(int sr, int sc, int dr, int dc, Board* b);
-};
-class KING :public PIECE
-{
-public:
-	KING(char s, COLOR c);
-	bool isValidMove(int sr, int sc, int dr, int dc, Board* b);
->>>>>>> 1b4b5c14264eb06ea16d3c23656984d34882283c
-};
