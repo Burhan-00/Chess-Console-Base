70d663f72f48732705ffa024cda28f295547eeb3
Soban Janjoa <sobanjanjoa@gmail.com>
2026-05-15 17:59:03 +0500

    Refactor Board class and method parameter names

--- BOARD.h
+++ BOARD.h
@@ -1,25 +1,25 @@
 #pragma once
 #include"PIECE.h"
 #include<windows.h>
-void setColor(int text, int bg);
-void gotoxy(int x, int y);
+void setColor(int textColor, int backgroundColor);
+void gotoxy(int positionX, int positionY);
 using namespace std;
 class Board
 {
 private:
 	PIECE* board[8][8];
-	int size;
+	int boardSize;
 public:
 	Board();
 	void setupBoard();
-	void displayBoard(COLOR turn);
-	void movepiece(int sr, int sc, int dr, int dc);
-	bool is_Path_Clear(int sr, int sc, int dr, int dc);
+	void displayBoard(COLOR currentTurn);
+	void movepiece(int sourceRow, int sourceCol, int destRow, int destCol);
+	bool is_Path_Clear(int sourceRow, int sourceCol, int destRow, int destCol);
 	bool is_King_Alive();
-	void find_King(COLOR c, int& row, int& col);
-	bool is_Check(COLOR c);
-	bool is_Check_Mate(COLOR c);
-	bool check_safety(int sr, int sc, int dr, int dc, COLOR c);
-	PIECE* get_piece(int r, int c);
+	void find_King(COLOR kingColor, int& kingRow, int& kingCol);
+	bool is_Check(COLOR kingColor);
+	bool is_Check_Mate(COLOR kingColor);
+	bool check_safety(int sourceRow, int sourceCol, int destRow, int destCol, COLOR kingColor);
+	PIECE* get_piece(int pieceRow, int pieceCol);
 	~Board();
 };
